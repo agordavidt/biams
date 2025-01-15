b6de110773840c11b5a70b7571c5b74399681945
David Agor <agordavid14@gmail.com>
2025-01-15 06:27:47 +0100

    complete database design for application practice

--- biams/database/migrations/2025_01_14_165508_create_animal_farmers_table.php
+++ biams/database/migrations/2025_01_14_165508_create_animal_farmers_table.php
@@ -14,33 +14,18 @@ return new class extends Migration
         Schema::create('animal_farmers', function (Blueprint $table) {
             $table->id();
              $table->foreignId('user_id')->constrained()->onDelete('cascade'); 
-             $table->foreignId('livestock_id')->constrained()->onDelete('cascade');          
+            //  $table->foreignId('livestock_id')->constrained()->onDelete('cascade');  
+
             // Demographic fields
-            $table->string('phone_number');
-            $table->date('date_of_birth');
-            $table->enum('gender', ['Male', 'Female']);
-            $table->enum('education_level', [
-                'No formal school',
-                'Primary school',
-                'Secondary school',
-                'Undergraduate',
-                'Graduate',
-                'Post Graduate'
-            ]);
-            $table->integer('household_size');
-            $table->integer('dependents');
-            $table->enum('income_level', [
-                'Less than N100,000',
-                'N100,001 - N250,000',
-                'N250,001 - N500,000',
-                'N500,001 - N1,000,000',
-                'Above N1,000,000'
-            ]);
-            $table->enum('local_government_area', [
-                'Ado', 'Agatu', 'Apa', 'Buruku', 'Gboko', 'Guma', 'Gwer East', 'Gwer West', 
-                'Katsina-Ala', 'Konshisha', 'Kwande', 'Logo', 'Makurdi', 'Obi', 'Ogbadibo', 
-                'Oju', 'Ohimini', 'Okpokwu', 'Otpo', 'Tarka', 'Ukum', 'Ushongo', 'Vandeikya' 
-            ]);
+            $table->string('phone')->nullable(false);
+            $table->date('dob')->nullable(false);
+            $table->string('gender')->nullable(false);
+            $table->string('education')->nullable(false);
+            $table->integer('household_size')->nullable(false);
+            $table->integer('dependents')->nullable(false);
+            $table->string('income_level')->nullable(false);
+            $table->string('lga')->nullable(false);
+
              // Animal Farmer  fields
             $table->json('livestock')->nullable(); 
             $table->integer('herd_size')->nullable(false); 
---
+++ biams/database/migrations/2025_01_15_051317_create_animal_farmer_livestock_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animal_farmer_livestock', function (Blueprint $table) {
+            $table->id();          
+            $table->foreignId('animal_farmer_id')->constrained()->onDelete('cascade');
+            $table->foreignId('livestock_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animal_farmer_livestock');
+    }
+};
