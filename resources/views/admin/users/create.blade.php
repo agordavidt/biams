d3e8cfb7057ba577b1691d6351a4d49cd2d2764c
David Agor <agordavid14@gmail.com>
2025-02-01 16:27:59 +0100

    tables

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,5 +1,10 @@
+@extends('layouts.table')
 
-<div class="container">
+@section('content')
+
+
+
+<!-- <div class="container">
     <h1>Add User</h1>
     <form action="{{ route('admin.users.store') }}" method="POST">
         @csrf
@@ -28,4 +33,67 @@
         </div>
         <button type="submit" class="btn btn-primary">Create User</button>
     </form>
-</div>
+</div> -->
+
+            <div class="row">
+                <div class="col-xl-6 mx-auto">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">Add User</h4>
+                            <p class="card-title-desc"></p>
+
+                           <form class="custom-validation" action="{{ route('admin.users.store') }}" method="POST">
+                                @csrf
+                                <div class="mb-3">
+                                    <label>Name</label>
+                                    <input type="text" class="form-control" name="name" required placeholder="Enter full name"/>
+                                </div>
+
+                                <div class="mb-3">
+                                    <label>E-Mail</label>
+                                    <div>
+                                        <input type="email" class="form-control" name="email" required parsley-type="email" placeholder="Enter a valid e-mail"/>
+                                    </div>
+                                </div>
+
+                                <div class="mb-3">
+                                    <label>Password</label>
+                                    <div>
+                                        <input type="password" id="pass2" class="form-control" name="password" required placeholder="Password"/>
+                                    </div>
+                                    <div class="mt-2">
+                                        <input type="password" class="form-control" name="password_confirmation" required data-parsley-equalto="#pass2" placeholder="Confirm Password"/>
+                                    </div>
+                                </div>
+
+                                <div class="mb-3">
+                                    <label>Role</label>
+                                    <div>
+                                        <select class="form-select" name="role" required>
+                                            <option selected disabled>Select role</option>
+                                            <option value="admin">Admin</option>
+                                            <option value="user">User</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="mb-0">
+                                    <div>
+                                        <button type="submit" class="btn btn-primary waves-effect waves-light me-1">
+                                            Create User
+                                        </button>
+                                        <button type="reset" class="btn btn-secondary waves-effect">
+                                            Cancel
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+
+
+                        </div>
+                    </div>
+                </div> <!-- end col -->
+
+            </div> <!-- end row -->
+
+@endsection
---
+++ resources/views/partials/crop-farming.blade.php
@@ -0,0 +1,23 @@
+
+    <!--  Modal content for the above example -->
+    <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="myLargeModalLabel">{{ $application->user->name }}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p><strong>: </strong></p>
+                    <p>Praesent commodo cursus magna, vel scelerisque
+                        nisl consectetur et. Vivamus sagittis lacus vel
+                        augue laoreet rutrum faucibus dolor auctor.</p>
+                    <p class="mb-0">Aenean lacinia bibendum nulla sed consectetur.
+                        Praesent commodo cursus magna, vel scelerisque
+                        nisl consectetur et. Donec sed odio dui. Donec
+                        ullamcorper nulla non metus auctor
+                        fringilla.</p>
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
